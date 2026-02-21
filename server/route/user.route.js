afe89c84b3984c0bb9d4c9eea274fb7a197daa79
suraj480 <surajmaurya991@gmail.com>
2026-02-21 22:33:19 +0530

    final server

--- server/route/user.route.js
+++ server/route/user.route.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
-import { registerUserController } from "../controller/user.controller.js"
+import { loginController, logoutController, registerUserController, verifyEmailController } from "../controller/user.controller.js"
 
 const userRouter = Router();
 
 userRouter.post("/register", registerUserController);
+userRouter.post('/verify-email',verifyEmailController)
+userRouter.post('/login',loginController)
+// userRouter.get('/logout',auth,logoutController)
 export default userRouter;
