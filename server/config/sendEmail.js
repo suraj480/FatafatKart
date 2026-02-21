0fa29bf50bf7f64c5637635d4bd6b81b68af1445
suraj480 <surajmaurya991@gmail.com>
2026-02-21 22:06:22 +0530

    register

--- server/config/sendEmail.js
+++ server/config/sendEmail.js
@@ -6,24 +6,28 @@ if (!process.env.RESEND_API_KEY) {
     console.log("Provide RESEND_API in side the .env file")
 }
 
-const resend = new Resend(process.env.RESEND_API);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async ({ sendTo, subject, html }) => {
     try {
         const { data, error } = await resend.emails.send({
-            from: 'fatafatcart <noreply@surajmaurya991@gmail.com>',
+            from: 'FatafatCart <onboarding@resend.dev>',
             to: sendTo,
-            subject: subject,
-            html: html,
+            subject,
+            html,
         });
 
         if (error) {
-            return console.error({ error });
+            console.log("Resend Error:", error);
+            throw new Error(error.message);
         }
 
-        return data
+        console.log("Email sent:", data);
+        return data;
+
     } catch (error) {
-        console.log(error)
+        console.log("SendEmail Catch Error:", error);
+        throw error;
     }
 }
 
